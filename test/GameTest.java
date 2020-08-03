e226c3153ee3421f4c5a6fd0441b40fb5bfb5316
akrishnan93 <anand.krrishnan@gmail.com>
2020-08-03 15:32:18 -0400

    Initial commit of Chess2D project.

--- test/GameTest.java
+++ test/GameTest.java
@@ -1,10 +1,9 @@
+import java.util.Arrays;
+
 public class GameTest {
 
     public static void main(String[] args) {
         ChessGame testGame = new ChessGame();
-
-        testGame.testGame();
-
-
     }
 }
+
